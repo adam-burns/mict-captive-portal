3ce14d1f384dd31468bc07d98b4a3acc39ec9f94
Pocket FM <pfm@localhost>
2020-12-04 13:09:16 +0000

    adds lighhptd service enabling

--- install.sh
+++ install.sh
@@ -7,7 +7,11 @@ mount -o rw,remount /
 cp -p /usr/sbin/dnsmasq /usr/sbin/dnsmasq.bin
 cp dnsmasq.sh /usr/sbin/dnsmasq
 chmod a+x /usr/sbin/dnsmasq
-mount -o ro,remount /
 touch /home/pfm/pfm_conf/dnsmasq.CAPTIVE_PORTAL
 echo "dnsmasq shell interceptor for NetworkManager installed"
+systemctl enable lighttpd
+echo "lighhtpd enabled"
+mount -o ro,remount /
+echo "lighhtpd started"
+systemctl start lighttpd
 echo "delete /home/pfm/pfm_conf/dnsmasq.CAPTIVE_PORTAL to stop captive portal"
