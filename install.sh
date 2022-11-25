f16204c1a58bb648a3851a00780cc13ea28dfd5a
Adam Burns <adam@dyne.org>
2022-11-25 23:39:09 +0100

    adds access.log

--- install.sh
+++ install.sh
@@ -11,13 +11,14 @@ touch /home/pfm/pfm_conf/dnsmasq.CAPTIVE_PORTAL
 echo "dnsmasq shell interceptor for NetworkManager installed"
 systemctl enable lighttpd
 echo "lighhtpd enabled"
+
 mount -o ro,remount /
 su - pfm -c "mkdir -p /home/pfm/pfm_store/var/log/lighttpd"
 su - pfm -c "mkdir -p /home/pfm/pfm_store/var/www"
 chown www-data /home/pfm/pfm_store/var/log/lighttpd
 cp -pr var/www/* /home/pfm/pfm_store/var/www
 chown -R pfm:pfm /home/pfm/pfm_store/var/www
-sed -e s/pfm009.local/${HOSTNAME}.local/g var/www/html/index.html >/home/pfm/pfm_store/var/www/html/index.html
+sed -e 's#accesslog.filename = "/var/log/lighttpd/access.log"#accesslog.filename = "/home/pfm/pfm_store/var/log/lighttpd/access.log"#g' /etc/lighttpd/conf-available/10-accesslog.confsed -e s/pfm009.local/${HOSTNAME}.local/g var/www/html/index.html >/home/pfm/pfm_store/var/www/html/index.html
 sed -e s/pfm009.local/${HOSTNAME}.local/g var/www/html/index.php >/home/pfm/pfm_store/var/www/html/index.php
 echo "example content loaded"
 systemctl start lighttpd
